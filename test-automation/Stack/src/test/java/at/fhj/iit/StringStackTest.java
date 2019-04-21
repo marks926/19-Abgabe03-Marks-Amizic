bb53e1eb0758086b3d2395edf67e45f48da27e90
Daniel Marks <daniel.marks@edu.campus02.at>
2019-04-21 16:25:19 +0200

    added new testmethod testPushFullStack()

--- test-automation/Stack/src/test/java/at/fhj/iit/StringStackTest.java
+++ test-automation/Stack/src/test/java/at/fhj/iit/StringStackTest.java
@@ -120,4 +120,16 @@ public class StringStackTest
     {
         s.pop();
     }
+    
+    //try to push an element onto a full stack
+    @Test(expected = IllegalStateException.class)
+    public void testPushFullStack() throws Exception
+    {
+        s.push("1");
+        s.push("2");
+        s.push("3");
+        s.push("4");
+        s.push("5");
+        s.push("6");
+    }
 }
